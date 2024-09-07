14b78cf17efead4870c12f26cb760c917b7a9f88
Meet2054 <meetaparekh2003@gmail.com>
2024-09-07 23:53:32 +0530

    setting routes in group routing basic set up

---
+++ web/app/(root)/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <main>
+      <div className="p-4" >
+        <Button> Click Me</Button>
+      </div>
+    </main>
+  );
+}
